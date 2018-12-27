c466632049171d4737ebce7b2a155a86d8462d0f
YasinEhsan <yasinehsan11@gmail.com>
2018-12-27 06:55:13 +0300

    distinguied func

--- allStar.java
+++ allStar.java
@@ -1,5 +1,24 @@
 class allStar{
     public static void main(String[] roar){
-        System.out.println("jhf");
+
+        String[] words = {"gin", "zen", "gig", "msg", 
+        "gin", "zen", "gig", "msg","gin", "zen", "gig", "msg"};
+        long startTime = System.nanoTime();
+        System.out.println(distinguish(words));
+        long duration = System.nanoTime() - startTime;
+        // System.out.println(duration);
+
     }
+
+    public static int distinguish(String[] words){
+        int count = 0;
+        for(int i = 0; i < words.length; ++i){
+            for(int j = i; j < words[i].length(); j++){
+                if(!words[i].equals(words[j]))
+                    count++;
+            }
+        }
+         return count;
+    }
+   
 }
