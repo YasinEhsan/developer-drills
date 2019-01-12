05e670631a2d45033358980856933b05f1ba0d23
YasinEhsan <yasinehsan11.com>
2019-01-12 09:57:22 -0500

    added bp '

---
+++ boilerplate.java
@@ -0,0 +1,20 @@
+import java.util.*; // i.e Arrays.sort
+import java.io.*; 
+public class {
+     /*QUESTION
+      */
+    public static void main( String[] args){
+        System.out.println("kjhwguefjgh");
+    }
+    public static void sol(){
+         /* P L A N 
+          -run time approx: 
+        */
+
+
+        /* FUTURE ITERATION
+        */
+        
+    }
+    
+}
