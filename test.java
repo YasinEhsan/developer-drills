2ca1a3d133a5e0ccd3e9824ccb29de9749d8a3af
YasinEhsan <yasinehsan11.com>
2019-02-26 14:14:23 -0500

    added test file

---
+++ test.java
@@ -0,0 +1,30 @@
+/* 
+Yasin Ehsan
+Coding Workouts 
+
+*/
+
+import java.util.*; // i.e Arrays.sort
+import java.io.*; 
+public class test {
+     /*QUESTION
+      */
+    public static void main( String[] args){
+        System.out.println("Run Successful!");
+        Scanner read = new Scanner(System.in);
+
+        String s = read.nextLine();
+        System.out.println(s);
+    }
+    public static void sol(){
+         /* P L A N 
+          -run time approx: 
+        */
+
+
+        /*FUTURE ITERATION
+        */
+        
+    }
+    
+}
