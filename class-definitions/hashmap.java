7510578d7e8182957206948845a01949cc815e4c
Yasin Ehsan <yasinehsan11@gmail.com>
2019-04-27 11:25:28 -0400

    Update hashmap.java

--- class-definitions/hashmap.java
+++ class-definitions/hashmap.java
@@ -9,37 +9,33 @@ public class GFG
 	public static void main(String[] args) 
 	{ 
 	
-		HashMap<String, Integer> map = new HashMap<>(); 
-		
-		print(map); 
+		HashMap<String, Integer> map = new HashMap<>();  
 		map.put("vishal", 10); 
 		map.put("sachin", 30); 
 		map.put("vaibhav", 20); 
-		
-	
 		print(map); 
     
-    //update value given key
-    map.put(key, map.get(key) + 1);
-    
-    //If you're only interested in the keys, you can iterate through the keySet() of the map:
-    Map<String, Object> map = ...;
-    for (String key : map.keySet()) {
-        // ...
-    }
-    
-    If you only need the values, use values():
-    for (Object value : map.values()) {
-        // ...
-    }
+	 	//update value given key
+	    	map.put(key, map.get(key) + 1);
+
+	    	//If you're only interested in the keys, you can iterate through the keySet() of the map:
+	    	Map<String, Object> map = ...;
+		for (String key : map.keySet()) {
+		// ...
+	    	}
+
+	    	If you only need the values, use values():
+	    	for (Object value : map.values()) {
+			// ...
+	    	}	
 
 
-    Finally, if you want both the key and value, use entrySet():
-    for (Map.Entry<String, Object> entry : map.entrySet()) {
-        String key = entry.getKey();
-        Object value = entry.getValue();
-        // ...
-    }
+	    	Finally, if you want both the key and value, use entrySet():
+	    	for (Map.Entry<String, Object> entry : map.entrySet()) {
+			String key = entry.getKey();
+			Object value = entry.getValue();
+			// ...
+	    	}
 		
 	} 
 	
