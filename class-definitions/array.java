48f1f77ae89c792c3105bd7d5be04a14105c1fdd
Yasin Ehsan <yasinehsan11@gmail.com>
2019-04-29 18:48:59 -0400

    Create array.java

---
+++ class-definitions/array.java
@@ -0,0 +1,10 @@
+class main{
+  public static void main(String[] args){
+  
+    String[] s = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    
+    //convert array to arraylist
+    ArrayList<String> store = Arrays.asList(s);
+  }
+
+}
