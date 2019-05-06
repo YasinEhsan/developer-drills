2f82b2bc1f686dd27ef31e1edc17c7e8d02a9f68
Yasin Ehsan <yasinehsan11@gmail.com>
2019-05-06 16:13:14 -0400

    Create 27-pointers-remove-value.java

---
+++ leetcode/27-pointers-remove-value.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        /*PLAN
+        - loop thru nums to find number of occurences of VAL
+        - create int pointer j set to num.len - 1. 
+        - create another loop from 0 to num.len - occurences val
+        - when counter val switch with position j. 
+        - decrement j while next value of j is not val
+        
+        -  two pointer sol is da move
+        */
+        
+        
+    }
+}
