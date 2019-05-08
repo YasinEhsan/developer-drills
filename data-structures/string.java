865011f6ddfc1744a9cc984db68eb0abed58cd67
Yasin Ehsan <yasinehsan11@gmail.com>
2019-05-08 01:10:24 -0400

    added string methods

--- data-structures/string.java
+++ data-structures/string.java
@@ -1,10 +1,13 @@
 class String{
 
-//convert to char array
-this.toCharArray();
+String s = " This is My string";
 
-//convert char array to String
-String s = new String(the char array);
+s.trim(); //remove leading and trailing spaces
+s.indexOf("i"); //returns starting index of first occrence of substring input
+s.lastIndexOf("i"); //returns starting index of last occrence of substring input
+
+char[] c = s.toCharArray(); //convert to char array
+String s2 = new String(c); //convert char array to String
 
 
 }
