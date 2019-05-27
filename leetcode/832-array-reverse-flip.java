eac0794d5c048dae58df8391b70ad41e314665a6
Yasin Ehsan <yasinehsan11@gmail.com>
2019-05-27 16:07:50 -0400

    Create 832-array-reverse-flip.java

---
+++ leetcode/832-array-reverse-flip.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] A) {
+        //just do this for double loop
+         for(int i = 0; i <= a.length - i - 1; ++i){
+            int temp = a[i];
+           a[i] = 1- a[a.length - i - 1];
+           a[a.length - i - 1] = 1- temp;
+       }
+        
+    
+    }
+}
