8f898854a4abad2f8f04cebb746c89398974c021
Yasin Ehsan <yasinehsan11@gmail.com>
2019-05-28 14:59:10 -0400

    Update array.java

--- data-structures/array.java
+++ data-structures/array.java
@@ -7,6 +7,8 @@ class main{
     ArrayList<String> store = Arrays.asList(a); //convert array to arraylist
     Arrays.sort(a); //sort by ascii
     Arrays.toString(a); // prints all elements in array 
+    
+    Arrays.fill(a,0); //fill a with 0s
   }
 
 }
