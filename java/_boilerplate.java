2d708e08b49daac9823bbf71b88ef29131cebbca
yasinehsan <yasinehsan11@gmail.com>
2019-10-29 19:01:37 -0400

    rm java code

--- java/_boilerplate.java
+++ java/_boilerplate.java
@@ -1,26 +1,8 @@
-/* 
-Yasin Ehsan
-Coding Workouts
+/* Yasin Ehsan
+
+Problem Statement: 
+
+Link(s):
+-
 
 */
-
-import java.util.*; // i.e Arrays.sort
-import java.io.*;
-public class {
-     /*QUESTION
-      */
-    public static void main( String[] args){
-        System.out.println("Run Successful!");
-    }
-    public static void sol(){
-         /* P L A N
-          -run time approx:
-        */
-
-
-        /*FUTURE ITERATION
-        */
-
-    }
-
-}
