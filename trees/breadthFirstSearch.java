20e914d0e45b583665498afb1ad858f58099a012
yasinehsan <yasinehsan11@gmail.com>
2019-11-06 20:41:21 -0500

    edited code to rm brackets one liner

--- trees/breadthFirstSearch.java
+++ trees/breadthFirstSearch.java
@@ -5,32 +5,27 @@ class Node{
         List<Node> adj = new ArrayList<>();
     }
 
+    /*plan
+        - use queue
+        - store visited nodes in hashset
+        - loop while stack contains
+            - poll queue and save to cur
+            - if curr is not in visted add it print it
+            - loop thru adjecy matrix
+            - if not visited add it to stack
+     */
     public void bfsPrint(Node start){
-        /*plan
-            - use queue
-            - store visited nodes in hashset
-            - loop while stack contains
-                - poll queue and save to cur
-                - if curr is not in visted add it print it
-                - loop thru adjecy matrix
-                - if not visited add it to stack
-         */
-
         Queue<Node> queue = new LinkedList<>();
         Set<Node> visited = new HashSet<>();
         queue.add(start);
 
         while (!queue.isEmpty()){
             Node curr = queue.poll();
-            if(!visited.contains(curr)){
+            if(!visited.contains(curr))
                 visited.add(curr);
-                //do something
-            }
 
-            for(Node adjacent : curr.adj){
-                if(!visited.contains(adjacent)){
+            for(Node adjacent : curr.adj)
+                if(!visited.contains(adjacent))
                     queue.add(adjacent);
-                }
-            }
         }
     }
--- trees/depthFirstSearch.java
+++ trees/depthFirstSearch.java
@@ -6,31 +6,27 @@ class Node{
         List<Node> adj = new ArrayList<>();
     }
 
+    /*plan
+        - use cal stack or create a stack
+        - store visited nodes in hashset
+        - loop while stack contains
+            - pop stack and save to cur
+            - if curr is not in visted add it print it
+            - loop thru adjecy matrix
+            - if not visited add it to stack
+     */
     public void dfsPrint(Node start){
-        /*plan
-            - use cal stack or create a stack
-            - store visited nodes in hashset
-            - loop while stack contains
-                - pop stack and save to cur
-                - if curr is not in visted add it print it
-                - loop thru adjecy matrix
-                - if not visited add it to stack
-         */
-
         Stack<Node> stack = new Stack<>();
         Set<Node> visited = new HashSet<>();
 
         stack.push(start);
         while (!stack.isEmpty()){
             Node curr = stack.pop();
-            if(!visited.contains(curr)){
+            if(!visited.contains(curr))
                 visited.add(curr);
-                //do something
-            }
-            for(Node n: curr.adj){
-                if(!visited.contains(n)){
+
+            for(Node n: curr.adj)
+                if(!visited.contains(n))
                     stack.push(n);
-                }
-            }
         }
       }
