2a45e1f1c6e75692b940e722f7f7c813d2c9944e
yasinehsan <yasinehsan11@gmail.com>
2019-11-27 18:10:35 +0200

    added insert and delete

--- trees/binarySearchTree.java
+++ trees/binarySearchTree.java
@@ -1,11 +1,61 @@
 public class Node{
-        int val;
-        Node left, right;
+    int val;
+    Node left, right;
 
-        public Node(int val){
-            this.val = val;
-        }
+    public Node(int val){
+        this.val = val;
     }
+}
+//
+//    /*PLAN for delete node n
+//        - Case 1: n is a leaf (bottom)
+//        - Case 2: n has one child
+//        - Case 3: n has two child
+//
+//     */
+public Node delete(Node root, int rmKey){
+    if(root == null){
+        return root;
+    }
+
+    if(root.val < rmKey)
+        delete(root.right, rmKey);
+    else if(root.val > rmKey)
+        delete(root.left, rmKey);
+    else {
+        if(root.left == null)
+            return root.right;
+        else if(root.right == null)
+            return root.left;
+
+        root.val = minValue(root.right);
+        root.right = delete(root.right, root.val);
+    }
+    return root;
+}
+
+private int minValue(Node root){
+    int minV = 0;
+    while(root.left != null){
+        minV = root.left.val;
+        root = root.left;
+    }
+    return minV;
+}
+
+public Node insert(Node root, int key){
+    if(root == null){
+        root = new Node(key);
+        return root;
+    }
+
+    if(root.val < key)
+        insert(root.right, key);
+    else if(root.val > key)
+        insert(root.left, key);
+    return root;
+}
+
 
 public boolean search2(Node root, int key){
         if(root == null) return false;
@@ -14,17 +64,4 @@ public boolean search2(Node root, int key){
         if(root.val < key)
             return search2(root.right, key);
         return search2(root.left, key);
-    }
-
-
-    public void insert2(Node root, int given){
-        if(root == null){
-            root = new Node(given);
-            return;
-        }
-
-        if(root.val < given)
-            insert2(root.right, given);
-        else
-            insert2(root.left, given);
     }
