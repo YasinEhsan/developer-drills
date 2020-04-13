2e7037c0422878489058a65217a6d8e70c92d5a6
yasinehsan <yasinehsan11@gmail.com>
2020-04-13 06:02:08 -0400

    added undone prob

---
+++ solvedProblems/slidingWindow_smallestSubarrayWithGivenSum_grokking1.2.py
@@ -0,0 +1,26 @@
+def smallest_subarray_with_given_sum(s, arr):
+  # TODO: Write your code here
+    '''
+    - have starting index, have currLen, minLen
+    - one found then cut down others in while loop style
+    - update vars
+    '''
+    startIndex, currLen, minLen, currSum = 0,0,len(arr), 0
+
+    for endIndex in range(len(arr)):
+        currSum += arr[endIndex]
+        currLen += 1
+        print(endIndex, arr[endIndex], currSum)
+
+        while currSum >= s:
+            minLen = min(minLen, currLen)
+            currLen -=1
+            currSum -= arr[startIndex]
+            startIndex += 1
+
+    return minLen
+smallest_subarray_with_given_sum(8, [3, 4, 1, 1, 6])
+
+# Given an array of positive numbers and a positive number ‘S’, find the length of the smallest contiguous subarray whose sum is greater than or equal to ‘S’. Return 0, if no such subarray exists.
+
+# https://www.educative.io/courses/grokking-the-coding-interview/7XMlMEQPnnQ
