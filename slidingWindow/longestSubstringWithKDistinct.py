e25a7312e7f2e09e7b75867e42d66f3f03568064
yasinehsan <yasinehsan11@gmail.com>
2020-05-22 08:46:28 -0400

    added revison

--- slidingWindow/longestSubstringWithKDistinct.py
+++ slidingWindow/longestSubstringWithKDistinct.py
@@ -1,4 +1,27 @@
 
+# 5.21
+def longest_substring_with_k_distinct(str, k):
+  # TODO: Write your code here
+  windowStart, maxLen, d = 0,0, {}
+
+  for windowEnd in range(len(str)):
+    rightChar = str[windowEnd]
+    if rightChar not in d:
+      d[rightChar] = 0
+    d[rightChar] +=1
+
+    while len(d) > k:
+      leftChar = str[windowStart]
+      d[leftChar] -=1
+
+      if d[leftChar] == 0:
+        del d[leftChar]
+      windowStart +=1
+    maxLen = max(maxLen, windowEnd - windowStart +1)
+  return maxLen
+
+
+
 # 5.20 forgot to add max() in maxLen -- returned tuple
 def longest_substring_with_k_distinct(str, k):
   # TODO: Write your code here
