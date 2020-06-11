f9ae4049753ec23542340ac401548d69d3cda980
yasinehsan <yasinehsan11@gmail.com>
2020-06-11 18:26:55 -0400

    done: l3 bfs

--- treeDFS/sumOfPathNumbers.py
+++ treeDFS/sumOfPathNumbers.py
@@ -1,3 +1,50 @@
+# june 11
+# each level times ten adn add once we are at root add it to sum
+# oG : check none, right and left for true, return both
+# pattern : false case and true case for each rescursiv code
+
+# REVISED ATTEMPT
+def find_sum_of_path_numbers(root):
+  return find_specific_path(root, 0)
+
+def find_specific_path(currNode, allSum):
+
+  # false case
+  if currNode is None:
+    return 0
+
+  allSum = allSum * 10 + currNode.val
+
+  # trueCase
+  if currNode.right is None and currNode.left is None:
+    return allSum
+
+# add left and right sub tree
+  return find_specific_path(currNode.right, allSum) + find_specific_path(currNode.left, allSum)
+
+
+# FISRT ATTEMPT NO WORK
+def find_sum_of_path_numbers(root):
+  # TODO: Write your code here
+  allSum = 0
+  find_specific_path(root, 0, allSum)
+  return allSum
+
+
+def find_specific_path(currNode, pathSum, allSum):
+
+  if currNode is None:
+    return 0
+
+  pathSum*=10 += currNode.val
+
+  if currNode.left is None and currNode.right is None:
+    allSum += pathSum
+  else:
+    find_specific_path(currNode.right, pathSum, allSum)
+    find_specific_path(currNode.left, pathSum, allSum)
+
+
 # 5 30 20
 def find_sum_of_path_numbers(root):
   # TODO: Write your code here
@@ -20,4 +67,4 @@ def get_path_sum(currNode, totalSum):
   left = get_path_sum(currNode.right, totalSum )
   right = get_path_sum(currNode.left, totalSum)
 
-  return left + right 
+  return left + right
